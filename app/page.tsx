2abd0d07f452bb233081f2b858b530861a94cf4a
Tailan Patrick <tailanpatrick06@gmail.com>
2025-12-11 12:26:07 -0300

    adicionadas mais urls

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,13 @@
 import Banner from '@/components/Banner';
 import Header from '@/components/Header';
-import PostCard from '@/components/PostCard';
-
-import notebookImage from '@/public/notebook.png';
+import PostsCards from '@/components/PostsCards';
 
 export default function Home() {
 	return (
-		<div>
+		<div className="flex flex-col items-center">
 			<Header />
 			<Banner />
-			{/* <PostCard
-				imageUrl={notebookImage}
-				title="long estabilished"
-				slug="long-estabilished"
-				size="big"
-				date="May 20th 2020"
-				excerpt="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-			/> */}
+			<PostsCards />
 		</div>
 	);
 }
