990d70c24f68b049acba36d4861929b3f84844e6
Tailan Patrick <tailanpatrick06@gmail.com>
2025-12-17 16:53:29 -0300

    footer agora foi para o arquivo de layout.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,11 @@
 import Banner from '@/components/Banner';
-import Footer from '@/components/Footer';
-import Header from '@/components/Header';
 import PostsCards from '@/components/PostsCards';
 
 export default function Home() {
 	return (
 		<div className="flex flex-col items-center">
-			<Header />
 			<Banner />
 			<PostsCards />
-
-			<Footer />
 		</div>
 	);
 }
