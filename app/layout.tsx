2111da6b6ea552ce6839d5d043d74bbfb4b26c56
Tailan Patrick <tailanpatrick06@gmail.com>
2025-12-17 16:52:59 -0300

    ajustado layout.tsx com estrutura de navbar e rodapé

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 
 const inter = Inter({
 	subsets: ['latin'],
@@ -19,7 +21,11 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="pt-BR">
-			<body className={inter.className}>{children}</body>
+			<body className={inter.className}>
+				<Header />
+				{children}
+				<Footer />
+			</body>
 		</html>
 	);
 }
