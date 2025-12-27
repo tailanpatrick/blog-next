efa846006ef50fee5ed80c600d082ba216f83c88
Tailan Patrick <tailanpatrick06@gmail.com>
2025-12-27 10:57:26 -0300

    renomeado componente para melhor entendimento do seu funcionamento

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,34 +1,34 @@
 'use client';
 import Image from 'next/image';
 import logo from '@/public/logo.png';
-import SearchMenuWithSocials from './SearchMenuWithSocials';
+import SearchMenuWithSocials from './SearchMenuAndSocials';
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
 
 const Header = () => {
-	const [isSticky, setIsSticky] = useState(false);
+  const [isSticky, setIsSticky] = useState(false);
 
-	useEffect(() => {
-		const handleScroll = () => {
-			setIsSticky(window.scrollY > 80);
-		};
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsSticky(window.scrollY > 80);
+    };
 
-		window.addEventListener('scroll', handleScroll);
-		return () => window.removeEventListener('scroll', handleScroll);
-	}, []);
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
 
-	return (
-		<header
-			className={`w-full flex justify-around items-center bg-white z-50 transition-all duration-300 ${
-				isSticky ? 'fixed top-0 left-0 shadow-md' : 'relative'
-			}`}
-		>
-			<Link href="/">
-				<Image src={logo} alt="" width={50} />
-			</Link>
-			<SearchMenuWithSocials />
-		</header>
-	);
+  return (
+    <header
+      className={`w-full flex justify-around items-center bg-white z-50 transition-all duration-300 ${
+        isSticky ? 'fixed top-0 left-0 shadow-md' : 'relative'
+      }`}
+    >
+      <Link href="/">
+        <Image src={logo} alt="" width={50} />
+      </Link>
+      <SearchMenuWithSocials />
+    </header>
+  );
 };
 
 export default Header;
